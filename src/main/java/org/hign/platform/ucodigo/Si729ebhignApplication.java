f780a6a12e0870201a05fc8dd3915c7233fb5a0b
XaerickSReborn <u202213358@upc.edu.pe>
2024-11-21 16:56:28 -0500

    chore: initial commit

---
+++ src/main/java/org/hign/platform/ucodigo/Si729ebhignApplication.java
@@ -0,0 +1,13 @@
+package org.hign.platform.ucodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Si729ebhignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Si729ebhignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hign/platform/ucodigo/Si729ebhignApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hign.platform.ucodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Si729ebhignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
