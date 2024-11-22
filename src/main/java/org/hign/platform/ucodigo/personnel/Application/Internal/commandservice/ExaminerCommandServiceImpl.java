91d3e0c99ffb52dd18ef4e9c36977bf03b772564
XaerickSReborn <u202213358@upc.edu.pe>
2024-11-21 19:38:27 -0500

    feat(personnel): add application layer for personnel bounded context.

---
+++ src/main/java/org/hign/platform/ucodigo/personnel/Application/Internal/commandservice/ExaminerCommandServiceImpl.java
@@ -0,0 +1,32 @@
+package org.hign.platform.ucodigo.personnel.Application.Internal.commandservice;
+
+
+import org.hign.platform.ucodigo.personnel.Domain.Model.Aggregate.Examiner;
+import org.hign.platform.ucodigo.personnel.Domain.Model.Command.CreateExaminerCommand;
+import org.hign.platform.ucodigo.personnel.Domain.Services.ExaminerCommandService;
+import org.hign.platform.ucodigo.personnel.Infrastructure.persistence.jpa.repositories.ExaminerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ExaminerCommandServiceImpl implements ExaminerCommandService {
+
+    private final ExaminerRepository examinerRepository;
+
+    @Autowired
+    public ExaminerCommandServiceImpl(ExaminerRepository examinerRepository) {
+        this.examinerRepository = examinerRepository;
+    }
+
+
+    @Override
+    public Optional<Examiner> handle(CreateExaminerCommand command) {
+        if (examinerRepository.existsByNationalProviderIdentifier(command.nationalProviderIdentifier())){
+            throw new IllegalArgumentException("National provider already exists");
+        }
+        Examiner examiner = new Examiner(command);
+        return Optional.of(examinerRepository.save(examiner));
+    }
+}
