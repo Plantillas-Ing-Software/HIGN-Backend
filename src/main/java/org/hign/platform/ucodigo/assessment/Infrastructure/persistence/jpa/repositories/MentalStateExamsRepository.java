20d400721a016abb4ab7ad69d90b71c4e7d55ae9
XaerickSReborn <u202213358@upc.edu.pe>
2024-11-21 19:39:16 -0500

    feat(assessment): add infrastructure layer for assessment bounded context.

---
+++ src/main/java/org/hign/platform/ucodigo/assessment/Infrastructure/persistence/jpa/repositories/MentalStateExamsRepository.java
@@ -0,0 +1,14 @@
+package org.hign.platform.ucodigo.assessment.Infrastructure.persistence.jpa.repositories;
+
+
+import org.hign.platform.ucodigo.assessment.Domain.Model.Aggregate.MentalStateExams;
+import org.hign.platform.ucodigo.personnel.Domain.Model.ValueObject.NationalProviderIdentifier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MentalStateExamsRepository extends JpaRepository<MentalStateExams, Long> {
+
+    boolean existsByExaminerNationalProviderIdentifierAndPatientId(
+            NationalProviderIdentifier examinerNationalProviderIdentifier, Long patientId);
+}
