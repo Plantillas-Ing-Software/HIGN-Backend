ecf9fdcf6fdb1a3b3fbed72a764958f08b5cdd96
XaerickSReborn <u202213358@upc.edu.pe>
2024-11-24 19:58:40 -0500

    feat(assessment): add exception file.

---
+++ src/main/java/org/hign/platform/ucodigo/assessment/Domain/Exceptions/NationalProviderIdentifierException.java
@@ -0,0 +1,9 @@
+package org.hign.platform.ucodigo.assessment.Domain.Exceptions;
+
+public class NationalProviderIdentifierException extends RuntimeException {
+
+    public NationalProviderIdentifierException(String message) {
+        super(message);
+    }
+
+}
